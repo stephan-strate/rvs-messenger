8f88a665ff1927a84bec0caf47548df62c145206
Stephan Strate <development@famstrate.com>
2017-12-20 23:01:58 +0100

    java project structure

---
+++ src/com/messenger/Main.java
@@ -0,0 +1,14 @@
+package com.messenger;
+
+/**
+ * <p></p>
+ * @author Jan-Philip Richter
+ * @author Stephan Strate
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+}

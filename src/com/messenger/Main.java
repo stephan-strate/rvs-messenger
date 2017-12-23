1ecccdcd8d00ddcda097a254cf11955582a070a7
Stephan Strate <development@famstrate.com>
2017-12-23 12:02:23 +0100

    updating main method

--- src/com/messenger/Main.java
+++ src/com/messenger/Main.java
@@ -1,18 +1,46 @@
 package com.messenger;
 
-import com.messenger.console.DefaultConsole;
-
 /**
- * <p></p>
+ * <p>Messenger application.</p>
  * @author Jan-Philip Richter
  * @author Stephan Strate
  * @version 1.0
  */
 public class Main {
 
+    /**
+     * <p>Read input parameters and start peer/
+     * messenger from it. Expected parameters are
+     * (String) name and (int) port.
+     * All other combinations of parameters will
+     * cause the program to exit with errors.</p>
+     * @param args  expecting messenger.jar (String) name, (int) port
+     */
     public static void main (String[] args) {
-        System.out.println("Messenger wird gestartet.");
+        // check if all parameters are given
+        if (args.length == 2 && args[0] != null && args[1] != null) {
+            try {
+                // store name
+                String name = args[0];
+                // parse port to int
+                int port = Integer.parseInt(args[1]);
 
-        Application application = new Application("Stephan", 4567);
+                // start application/program
+                System.out.println("Messenger/Peer wird gestartet.");
+                Application application = new Application(name, port);
+            } catch (NumberFormatException e) {
+                System.out.println("Der Port muss eine valide Nummer sein.\n" +
+                        "Beispiel: messenger.jar Jon 6734");
+
+                // terminate application with errors
+                System.exit(1);
+            }
+        } else {
+            System.out.println("Es muss genau ein Name und ein valider Port übergebn werden.\n" +
+                    "Beispiel: messenger.jar Jon 6734");
+
+            // terminate application with errors
+            System.exit(1);
+        }
     }
 }
